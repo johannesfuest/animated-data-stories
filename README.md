c823bf6eb82a7757023bf269a4744167ba2e3c14
Nicolas Neudeck <nicolas.neudeck@outlook.de>
2021-11-20 13:53:17 +0100

    add outlier script and main func and load data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # animated-data-stories
 SAP challenge @hackatum 2021
+
+# Instructions
+Download the dataset from https://www.kaggle.com/berkayalan/retail-sales-data/code
+Insert the sales.csv file into the dataset directory
--- main.py
+++ main.py
@@ -1,23 +1,47 @@
+import pandas as pd
 from reports.store_outlier import get_store_outlier_for_yesterday
 from generate_video import generate_video
 
 YESTERDAY = "2018-09-17"
 
+def load_data():
+    # Load from csv
+    df_product_hierachy = pd.read_csv('dataset/product_hierarchy.csv', 
+                                    delimiter= ',',
+                                    dtype={'product_id':'category',
+                                            'cluster_id':'category',
+                                            'hierarchy1_id':'category',
+                                            'hierarchy2_id':'category',
+                                            'hierarchy3_id':'category',
+                                            'hierarchy4_id':'category',
+                                            'hierarchy5_id':'category'})
+    df_sales = pd.read_csv('dataset/sales.csv',
+                        delimiter=',', 
+                        dtype={"product_id":"category", 
+                                "store_id":"category",
+                                "promo_type_1":"category",
+                                "promo_bin_1":"category",
+                                "promo_type_2":"category",
+                                "promo_bin_2":"category",
+                                "promo_discount_2":"category",
+                                "promo_discount_type_2":"category"},
+                        parse_dates=["date"])
+    return df_sales.join(df_product_hierachy.set_index('product_id'), on='product_id')
+
 def main():
-    # load data
-    print("Load Date")
-
-    print("Data Loaded")
-
+    print("Load CSV Data")
+    df_sales = load_data()
+    print("CSV Data Loaded")
     # call different outlier scripts
     print("Check for Yesterdays Outliers")
-    yesterday_outliers = get_store_outlier_for_yesterday(YESTERDAY)
+    yesterday_outliers = get_store_outlier_for_yesterday(df_sales, YESTERDAY)
     print(f"Found {len(yesterday_outliers)} store outliers")
     # do priority
 
     # generate video
     print("Start generation Videos")
-    generate_video(yesterday_outliers)
+    video = generate_video(yesterday_outliers)
+
     # upload video
     print("Start Uploading Videos")
     print("Script Done")
