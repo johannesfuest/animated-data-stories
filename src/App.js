c0b37d05e04c46f1f97fd30ac3c659b7c08fe1a2
Thomas Boehm <thomas.boehm@tum.de>
2021-11-20 20:46:14 +0100

    Add controls

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
+import { useState } from 'react';
+
 import './App.css';
 import video1 from './story1.mp4';
+import video2 from './story2.mp4';
+
 import avatar from './avatar.svg';
+import left from './left.svg';
+import right from './right.svg';
+import help from './help.svg';
 
 export default function App() {
+  const videos = [video1, video2]
+  
+  const [videoIndex, setVideoindex] = useState(0)
+  
+  function nextVideo() {
+    if (videoIndex + 1 < videos.length) {
+      setVideoindex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  function prevVideo() {
+    if (videoIndex - 1 >= 0) {
+      setVideoindex(videoIndex - 1);
+    }
+  }
+
   return (
     <div className="app">
       <div className="header">
         <span>Data Stories</span>
         <div className="btn">
-          <img className="avatar" src={avatar} />
+          <img className="icn" src={avatar} />
         </div>
       </div>
       <div className="player">
-      <video width="100%" height="100%" autoPlay controls>
-          <source src={video1} type="video/mp4"/>
+        <video width="100%" height="100%" autoPlay controls key={videos[videoIndex]}>
+          <source src={videos[videoIndex]} type="video/mp4"/>
           Your browser does not support the video tag.
         </video>
+      </div>
+      <div className="controls">
+        <div className="btn">
+          <img className="icn" src={left} onClick={prevVideo} />
+        </div>
+        <div className="btn">
+          <a href='https://devpost.com/software/animated-data-stories'>
+            <img className="icn " src={help} />
+          </a>
+        </div>
+        <div className="btn">
+          <img className="icn" src={right} onClick={nextVideo} />
+        </div>
       </div>
     </div>
   );
