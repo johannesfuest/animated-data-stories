e216db69472f3749e8d36648631c2b9cae5952d9
Thomas Boehm <thomas.boehm@tum.de>
2021-11-20 18:56:25 +0100

    Add web app

---
+++ webapp/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import video1 from './story.mp4';
+import avatar from './avatar.svg';
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="header">
+        <span>Data Stories</span>
+        <div className="btn">
+          <img className="avatar" src={avatar} />
+        </div>
+      </div>
+      <div className="player">
+      <video width="100%" height="100%" autoPlay controls>
+          <source src={video1} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  );
+}
